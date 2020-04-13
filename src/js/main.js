e41c13b4c826e8e1b5666c82ee56cecb858e4b36
Adrianna Dziewulska <dziewulska37@gmail.com>
2020-04-13 13:06:28 +0200

    page on gulp

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,19 @@ if ('serviceWorker' in navigator) {
 // place your code below
 
 
-console.log(`Hello world!`);
+console.log(`Cześć! Jestem autorką tej strony, nazywam się Ada Dziewulska i mam 15 lat, miło, że tu zglądasz, a skoro to robisz to trochę się interesujesz JavaScript-em ;) Ja się już żegnam i życzę miłego dnia :D`); 
+
+const welcome = (name, age) => {
+    console.log(`Witaj ${name}, masz ${age} lat.`)
+};
+welcome(`Ada`, 15);
+
+const navigationSwitcher = document.querySelector (`.navigation__switcher--js`);
+
+navigationSwitcher.addEventListener('click', (e) => {
+    const navigationList = document.querySelector (`.navigation__list--js`);
+    navigationList.classList.toggle(`navigation__list--visible`);
+});
+
 
 
